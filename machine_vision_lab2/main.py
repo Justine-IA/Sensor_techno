35b2a19af80bccbacdd7cfd38882f4522e5cb2b8
Jean Justine <jeju0004@student.hv.se>
2024-12-12 11:42:17 +0100

    better

--- machine_vision_lab2/main.py
+++ machine_vision_lab2/main.py
@@ -27,7 +27,7 @@ def main():
 
         canny_contours = MyDetectionMethods.canny_filter(frame)
         canny_frame = frame.copy()
-        cv2.drawContours(frame, canny_contours, -1, (0, 0, 255), 2)
+        # cv2.drawContours(frame, canny_contours, -1, (0, 0, 255), 2)
 
 
         # Detect ArUco markers in the frame
@@ -52,17 +52,16 @@ def main():
 
             for contour in canny_contours:
                 x, y, w, h = cv2.boundingRect(contour)  # Get bounding box for each contour
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)  # Draw rectangle
-                centroid = (x + w // 2, y + h // 2)
-                cv2.circle(frame, centroid, radius=2, color=(0, 0, 255), thickness=-1)
                 width = round(w*pixel_to_cm_ratio, 1)
                 height = round(h*pixel_to_cm_ratio,1)
+                if width>1 and height>4:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)  # Draw rectangle
+                    centroid = (x + w // 2, y + h // 2)
+                    cv2.circle(frame, centroid, radius=2, color=(0, 0, 255), thickness=-1)
 
+                    text = f"Width:{width:.1f}, Height:{height:.1f}"
+                    cv2.putText(frame, text, (centroid[0], centroid[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
 
-                text = f"Width:{width}, Height:{height}"
-                cv2.putText(frame, text, (centroid[0], centroid[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1)
-
-        
 
         # Show the frame
         cv2.imshow("ArUco Marker Detection", frame)
